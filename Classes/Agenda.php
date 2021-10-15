d22f2aec4bf70caaf653e97f4c279c27345fb22e
israel sena <israelomaximo@hotmail.com>
2021-10-15 16:21:01 -0300

    Crud concluído

--- Classes/Agenda.php
+++ Classes/Agenda.php
@@ -15,33 +15,34 @@ Class Agenda extends Conexao{
         return $this->con->query($sql);
     }
     public function inserir($dados){
-        $nome = $dados["nome"];
-        $email = $dados["email"];
-        $fone = $dados["fone"];
+        $nome =   filter_var($dados["nome"],FILTER_SANITIZE_STRING);
+        $email =  filter_var($dados["email"],FILTER_SANITIZE_EMAIL);
+        $fone =  filter_var($dados["fone"],FILTER_SANITIZE_STRING);
         $insert = "insert into agenda (nome,email,fone) values ('$nome','$email','$fone')";
         
         $this->con->query($insert);
         
     }
     public function apagar($id_agenda){
-      
-        $apagar = "delete from agenda where id_agenda={$id_agenda}";
+        $codigo = filter_var($id_agenda,FILTER_SANITIZE_NUMBER_INT);
+        $apagar = "delete from agenda where id_agenda={$codigo}";
         
         $this->con->query($apagar);
         
     }
     public function obter($id_agenda){
-      
-        $query = "select * from agenda where  id_agenda={$id_agenda}";
+        $codigo = filter_var($id_agenda,FILTER_SANITIZE_NUMBER_INT);
+        $query = "select * from agenda where  id_agenda={$codigo}";
         
         return $this->con->query($query);
         
     }
     
      public function atualizar($dados){
-        $nome = $dados["nome"];
-        $email = $dados["email"];
-        $fone = $dados["fone"];
+      
+        $nome =   filter_var($dados["nome"],FILTER_SANITIZE_STRING);
+        $email =  filter_var($dados["email"],FILTER_SANITIZE_EMAIL);
+        $fone =  filter_var($dados["fone"],FILTER_SANITIZE_STRING);
         $id_agenda = $dados["id_agenda"];
         $query = "update agenda set nome='{$nome}',email='{$email}',fone='{$fone}' where  id_agenda={$id_agenda}";
       
